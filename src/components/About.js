de761f857d19a477b3207bb8d665c4c6529af606
Juan Pablo Zendejas <jp@zendejas.family>
2024-08-17 21:16:44 -0600

    Improve responsiveness of Officers

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { info } from "../data/meta";
 
 export default function About() {
 	return (
-		<section id="about" class="p-8">
+		<section id="about" class="p-8 pb-10">
 			<h1 class="text-4xl font-bold text-center m-4">About the ACM Chapter</h1>
 			<p class="m-4 mx-auto max-w-3xl text-lg" dangerouslySetInnerHTML={{ __html: info.description }}></p>
 		</section>
--- src/components/Officers.js
+++ src/components/Officers.js
@@ -3,9 +3,9 @@ import { officers } from "../data/officers";
 
 export default function Officers() {
 	return (
-		<section id="officers" class="p-8 bg-gray-900 text-gray-200">
+		<section id="officers" class="p-8 pb-10 bg-gray-900 text-gray-200">
 			<h1 class="text-4xl font-bold text-center m-4">Executive Committee</h1>
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center gap-4">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center gap-4 m-4">
 				{officers.map((officer) => (
 					<div key={officer.id} className="min-h-12 min-w-16 text-center text-gray-200 bg-gradient-to-r from-acm-dark-blue to-acm-dark-blue via-acm-blue p-4 rounded-lg flex flex-col items-center justify-center">
 						<img src={officer.photo} class="w-32 h-32 mb-1 rounded-xl"></img>
