3ce9f7e47f1bd00adbaef1809bf7ceb31b8d76fd
Juan Pablo Zendejas <jp@zendejas.family>
2024-08-17 21:06:44 -0600

    Improve responsiveness of Officers

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import { info } from "../data/meta";
 
 export default function About() {
 	return (
-		<section id="about" class="py-8">
+		<section id="about" class="p-8">
 			<h1 class="text-4xl font-bold text-center m-4">About the ACM Chapter</h1>
-			<p class="m-4 mx-auto px-4 max-w-3xl text-lg" dangerouslySetInnerHTML={{ __html: info.description }}></p>
+			<p class="m-4 mx-auto max-w-3xl text-lg" dangerouslySetInnerHTML={{ __html: info.description }}></p>
 		</section>
 	);
 }
--- src/components/Officers.js
+++ src/components/Officers.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { officers } from "../data/officers";
 
-export default function Officers(){
-    return (
-        <section id="officers"  class="py-4 bg-gray-900 text-gray-200 min-h-screen">
-            <h1 class="text-4xl font-bold text-center m-4">Executive Committee</h1>
-            <div class="flex flex-row space-x-4 p-4">
-                <div className="grid grid-cols-1 md:grid-cols-4 justify-center mx-auto gap-4 pl-8 pr-8">
-                    {officers.map((officer) => (
-                        <div key={officer.id} className="text-gray-200 bg-gradient-to-r from-acm-dark-blue to-acm-dark-blue via-acm-blue p-4 rounded-lg flex flex-col items-center justify-center">
-                            <img src={officer.photo} class="w-32 h-32 mb-1 rounded-xl"></img>
-                            <a href={officer.url ? officer.url : `mailto:${officer.email}`} target="_blank" class="text-2xl flex items-center justify-center space-x-2 font-bold">
-                                {officer.name}
-                                {/*(
+export default function Officers() {
+	return (
+		<section id="officers" class="p-8 bg-gray-900 text-gray-200">
+			<h1 class="text-4xl font-bold text-center m-4">Executive Committee</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center gap-4">
+				{officers.map((officer) => (
+					<div key={officer.id} className="min-h-12 min-w-16 text-center text-gray-200 bg-gradient-to-r from-acm-dark-blue to-acm-dark-blue via-acm-blue p-4 rounded-lg flex flex-col items-center justify-center">
+						<img src={officer.photo} class="w-32 h-32 mb-1 rounded-xl"></img>
+						<a href={officer.url ? officer.url : `mailto:${officer.email}`} target="_blank" class="text-2xl flex items-center justify-center space-x-2 font-bold">
+							{officer.name}
+							{/*(
                                 <img class="w-7 h-7 opacity-50" src="https://as.sdsu.edu/images/assets/assets-icons/email.svg" alt="Email Icon" />
                                 )*/}
-                            </a>
-                            <p class="text-xl">{officer.position}</p>
-                            <p class="text-center text-gray-200">{officer.major}</p>
-                            {/* <p class="text-center text-gray-400">Committees: {officer.committees.join(", ")}</p> */}
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+						</a>
+						<p class="text-xl">{officer.position}</p>
+						<p class="text-gray-200">{officer.major}</p>
+						{/* <p class="text-center text-gray-400">Committees: {officer.committees.join(", ")}</p> */}
+					</div>
+				))}
+			</div>
+		</section>
+	);
 }
