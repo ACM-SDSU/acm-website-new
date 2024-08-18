f26bbf1314fff30cacd01b5d51fa9f9a7053fab4
Juan Pablo Zendejas <jp@zendejas.family>
2024-08-17 20:30:35 -0600

    Improve responsiveness of About

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,11 @@
 import React from "react";
-import {info} from "../data/meta";
+import { info } from "../data/meta";
 
-export default function About(){
-    return (
-        <section id="about" class="py-8 h-screen">
-            <h1 class="text-4xl font-bold text-center m-4">About the ACM Chapter</h1>
-            <div class="flex flex-row space-x-4 p-4">
-                <div class="p-4 w-1/6">
-
-                </div>
-                <div class="p-4 w-2/3 text-lg">
-                    <p dangerouslySetInnerHTML={{__html: info.description}}></p>
-                </div>
-                <div class="p-4 w-1/6">
-
-                </div>
-            </div>
-        </section>
-    );
+export default function About() {
+	return (
+		<section id="about" class="py-8">
+			<h1 class="text-4xl font-bold text-center m-4">About the ACM Chapter</h1>
+			<p class="m-4 mx-auto px-4 max-w-3xl text-lg" dangerouslySetInnerHTML={{ __html: info.description }}></p>
+		</section>
+	);
 }
