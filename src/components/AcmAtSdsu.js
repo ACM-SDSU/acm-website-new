80989399fbd1dcb20030f46fc7bfc734d2c29d66
EverettRichards <ehrichards9@gmail.com>
2024-08-19 22:06:57 -0700

    Added mouseover effect for instances of ACM@SDSU

--- src/components/AcmAtSdsu.js
+++ src/components/AcmAtSdsu.js
@@ -1,17 +1,22 @@
 import React from "react";
+import ILink from "./Link";
 
 export function AcmAtSdsu({ children }) {
 	return (
-        <span class="acm-at-sdsu font-bold">
-            <span class="text-acm-light-blue">ACM</span> @<span class="text-acm-red">SDSU</span>
-        </span>
+        <ILink to="overview">
+            <span class="acm-at-sdsu font-bold">
+                <span class="text-acm-light-blue">ACM</span> @<span class="text-acm-red">SDSU</span>
+            </span>
+        </ILink>
 	);
 }
 
 export function AcmAtSdsuLightBg({ children }) {
 	return (
-        <span class="acm-at-sdsu font-bold">
-            <span class="text-acm-blue">ACM</span> @<span class="text-acm-red">SDSU</span>
-        </span>
+        <ILink to="overview">
+            <span class="acm-at-sdsu font-bold hover:text-yellow-500">
+                <span class="text-acm-blue">ACM</span> @<span class="text-acm-red">SDSU</span>
+            </span>
+        </ILink>
 	);
 }
