fb7d6cf14581d1b7f143788e3ded9085052fb525
EverettRichards <ehrichards9@gmail.com>
2024-09-21 16:18:35 -0700

    Added youtube to contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import { AcmAtSdsuLightBg } from "./AcmAtSdsu";
 export default function Contact(){
     return (
         <section id="contact" class="pt-6 pb-12">
-            <h1 class="text-4xl font-bold text-center m-4">Contact Us</h1>
+            <h1 class="text-4xl font-bold text-center m-4">Get Involved</h1>
             <p class="text-center text-xl m-4 p-4">The <AcmAtSdsuLightBg /> welcomes you to reach out at any time via Discord, Instagram, or Email.</p>
             <div class="flex flex-row flex-wrap space-x-4 m-2 lg:m-4 justify-center text-2xl text-center">
                 <div class="p-4 min-w-1/5">
@@ -26,7 +26,8 @@ export default function Contact(){
                     </a>
                 </div>
             </div>
-            <p class="text-center text-xl m-4 p-4">You can also catch us at our weekly chapter meeting at [LOCATION TBD] from 4:00-5:00pm.</p>
+            <p class="text-center text-xl m-4 p-4">You can also catch us at our weekly chapter meeting every Wednesday from 4:00-5:00pm in GMCS 408.</p>
+            <p class="text-center text-xl m-4 p-4 italic">If you miss a meeting, you can always catch up by watching our recorded lectures and workshops on our <a href="https://www.youtube.com/@SDSUACM/videos" target="_blank" class="underline">YouTube Channel</a>.</p>
         </section>
     );
 }
