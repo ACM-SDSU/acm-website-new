5452959a46094063db9687117a03e3deb136df12
EverettRichards <ehrichards9@gmail.com>
2024-11-06 16:32:53 -0800

    I love animals!!!

---
+++ src/data/fav_animals/everett.js
@@ -0,0 +1,7 @@
+export const everett_animals = {
+    "name": "computer scientist",
+    "description": "antisocial",
+    "type": "mammal (barely)",
+    "height": "5FT 11IN",
+    "weight": 500
+};
