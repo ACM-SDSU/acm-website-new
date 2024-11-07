7a92022d8265fd9f4203fcc38183acb1f3a58d36
MIGUEL <anthonymiguelochoa03@gmail.com>
2024-11-06 16:47:59 -0800

    hi

--- src/data/fav_animals/miguel.js
+++ src/data/fav_animals/miguel.js
@@ -3,5 +3,7 @@ export const jp_animals = {
     "description": "delicious",
     "size": "Around 9 to 10 inches in diameter.",
     "height": "Typically about 1.5 to 2 inches deep.",
-    "price]": "$12.75"
+    "price]": "$12.75",
+    "entree": "teriyaki chicken",
+    "side": "white rice"
 };
