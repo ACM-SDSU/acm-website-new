517037ffdfe88c65f1010047f5759ea96a13128a
Nathan S <Nshorez8219@sdsu.edu>
2024-11-06 16:47:37 -0800

    Commit

---
+++ src/data/fav_animals/Nathan.js
@@ -0,0 +1,9 @@
+export const jp_animals = {
+    "name": "Fluffles",
+    "Affiliation": "The Mafia (Formerly), Knights of the Blood Oath (Formerly), The King of Ashes",
+    "Type": "mammal",
+    "Height": "9In",
+    "Weight": "9lb",
+    "Likes": "Torture, Gladitorial Combat, Pictionary",
+    "Dislikes": "Debtors"
+};
